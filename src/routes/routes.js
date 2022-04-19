26d2f9fecdd362b3950f9059dd63fb3e4d50a076
rafaelreisramos <rrramos@gmail.com>
2022-04-19 14:18:49 -0300

    chore: alive and 404 routes handler

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,25 @@
+import { URL } from "node:url"
+
 const handler = (req, res) => {
-  res.end("Hello world")
+  const url = new URL(`${req.protocol}//${req.headers.host}${req.url}`)
+  const pathname = url.pathname.replace(/^\//, "")
+
+  const chosenHandler = routes[pathname] ?? routes["notFound"]
+
+  chosenHandler((status) => {
+    res.writeHead(status).end()
+    console.log("Returning this response:", status)
+  })
+}
+
+const routes = {
+  alive: (callback) => {
+    callback(200)
+  },
+
+  notFound: (callback) => {
+    callback(404)
+  },
 }
 
 export default handler
