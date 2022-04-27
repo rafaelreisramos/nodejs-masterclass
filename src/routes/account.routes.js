3043a7174cdf97264f05f552b8643a47c145590d
rafaelreisramos <rrramos@gmail.com>
2022-04-27 18:20:32 -0300

    chore: add account creation page and route

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,3 +1,41 @@
-export default (_, callback) => {
-  callback(404)
+import _data from "../lib/data.js"
+import helpers from "../utils/helpers.js"
+
+const routes = (data, callback) => {
+  const methods = ["get"]
+  if (!methods.includes(data.method)) {
+    return callback(405)
+  }
+  handler[data.method](data, callback)
 }
+
+const handler = {}
+
+handler.get = async function (data, callback) {
+  const templateData = {
+    "head.title": "Create an account",
+    "head.description": "Signup is easy and only takes a few seconds.",
+    "body.class": "index",
+  }
+
+  let document = ""
+  try {
+    const page = await helpers.getPageTemplate(
+      "account-create.html",
+      templateData
+    )
+    if (!page.length) {
+      throw new Error()
+    }
+    document = await helpers.documentTemplate(page, templateData)
+    if (!document.length) {
+      throw new Error()
+    }
+  } catch {
+    return callback(500)
+  }
+
+  callback(200, document, "text/html")
+}
+
+export default routes
